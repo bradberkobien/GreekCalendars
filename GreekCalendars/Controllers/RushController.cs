02d0c9409a534347a4c3205aa4d474ec3bb2bfdf
Brad <berk6529@kettering.edu>
2017-02-13 12:50:10 -0500

    - Made everything functional for SigmaAlphaEpsilon Calendars (View and Compare buttons)

--- GreekCalendars/Controllers/RushController.cs
+++ GreekCalendars/Controllers/RushController.cs
@@ -8,9 +8,51 @@ namespace GreekCalendars.Controllers
 {
     public class RushController : Controller
     {
+
         // GET: Rush
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult CheckSubmit(string submitButton)
+        {
+            switch (submitButton)
+            {
+                case "View":
+                    // delegate sending to another controller action
+                    return (ViewCalendar());
+                case "Compare":
+                    // call another action to perform the cancellation
+                    return (CompareCalendar());
+                default:
+                    // If they've submitted the form without a submitButton, 
+                    // just return the view again.
+                    return (View());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ViewCalendar()
+        {
+            string sigmaAlphaEpsilon = this.Request.Form["sigmaAlphaEpsilon"];
+            if(sigmaAlphaEpsilon == "on")
+            {
+                return View("SigmaAlphaEpsilonCalendar");
+            }
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CompareCalendar()
+        {
+            string sigmaAlphaEpsilon = this.Request.Form["sigmaAlphaEpsilon"];
+            if (sigmaAlphaEpsilon == "on")
+            {
+                return View("SigmaAlphaEpsilonCalendar");
+            }
+
             return View();
         }
     }
